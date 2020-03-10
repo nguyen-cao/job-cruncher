e2df0afe0b5282d169f2e45dce7c218f9b4e284e
Sanjana12111994 <sanjanarchauhan@gmail.com>
2020-03-10 15:33:50 -0700

    update

--- collectors/collectors/pipelines.py
+++ collectors/collectors/pipelines.py
@@ -11,6 +11,8 @@ from datetime import datetime
 
 PIPELINE_SPIDERS = {
     'IndeedJobListCollectorPipeline': ['indeed-job-list'],
+    'GlassdoorJobListCollectorPipeline': ['glassdoor-job-list']
+
 } 
 
 class IndeedJobListCollectorPipeline(object):
@@ -36,5 +38,31 @@ class IndeedJobListCollectorPipeline(object):
             source=item['source'])
         
         self.session.add(jobPost)
+        self.session.commit()
+        return item
+
+class GlassdoorJobListCollectorPipeline(object):
+    def __init__(self):
+        self.session = create_session()
+
+    def open_spider(self, spider):
+        print('Start spider...'+spider.name)
+
+    def close_spider(self, spider):
+        print('Stop spider... '+spider.name)
+        close_session(self.session)
+
+    def process_item(self, item, spider):
+        spider_name = spider.name
+        if spider_name not in PIPELINE_SPIDERS[self.__class__.__name__]:
+            return item
+
+        jobPost_glassdoor = JobPost_glassdoor(title=item['title'], \
+            company=item['company'], \
+            location=item['location'], \
+            description=item['description'], \
+            source=item['source'])
+        
+        self.session.add(jobPost_glassdoor)
         self.session.commit()
         return item
