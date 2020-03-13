49f120938ccc62eec5b38935a84aa8e2a2ef8e9c
Sanjana12111994 <sanjanarchauhan@gmail.com>
2020-03-12 21:26:16 -0700

    Fixed pagination

--- collectors/collectors/spiders/glassdoor-job-list.py
+++ collectors/collectors/spiders/glassdoor-job-list.py
@@ -9,17 +9,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
 from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
 from selenium.webdriver.chrome.options import Options
 
 from ..models.job import create_session
 from ..items import GlassdoorJobItem
 
-MAX_JOBS = 20
+MAX_JOBS = 50
 
 class GlassdoorJobListSpider(scrapy.Spider):
     name = 'glassdoor-job-list'
-    allowed_domains = ['glassdoor.com']
+    allowed_domains = ['glassdoor.ca']
     start_urls = ['https://www.glassdoor.ca/Job']
     
 
@@ -39,40 +39,53 @@ class GlassdoorJobListSpider(scrapy.Spider):
         chrome_options = Options()  
         driver = webdriver.Chrome(chrome_options=chrome_options)
         try:
-            #driver.get('chrome://settings/')
-            #driver.execute_script('chrome.settingsPrivate.setDefaultZoom(0.70);')
             driver.get(url)
             
-            job_containers = driver.find_elements_by_css_selector('.jlGrid.hover')
+            job_containers = driver.find_elements_by_css_selector('.jobContainer')
             for job_element in job_containers:
                 self.jobs_scraped += 1
                 if (self.jobs_scraped > MAX_JOBS):
                     return
 
-                
-                job_title = job_element.find_element_by_css_selector('.jobLink.jobInfoItem.jobTitle').text    
-                job_company = job_element.find_element_by_css_selector('.jobInfoItem.jobEmployerName').text
-                
-                
+                try:
+                    driver.find_element_by_class_name("selected").click()
+                except ElementClickInterceptedException:
+                    pass
 
-                job_link = job_element.find_element_by_css_selector('li.jl')
+                time.sleep(.1)
+
+                # Trying to bypass Glassdoor login
+                try:
+                    driver.find_element_by_css_selector(".SVGInline.modal_closeIcon").click()  #clicking to the X.
+                except NoSuchElementException:
+                    pass
+
+                job_title = job_element.find_element_by_css_selector('.jobLink.jobInfoItem.jobTitle').text    
+                job_company = job_element.find_element_by_css_selector('.jobInfoItem.jobEmpolyerName').text
+                job_location = job_element.find_element_by_css_selector('.loc').text
+
+                job_link = job_element.find_element_by_css_selector('.jobLink.jobInfoItem.jobTitle')
                 job_link.click()
                 time.sleep(2)
-                job_location = job_element.find_element_by_css_selector('.location').text
+                
                 job_description = driver.find_element_by_css_selector('.jobDescriptionContent.desc').text
 
                 job_item = GlassdoorJobItem()
+                # job_item = IndeedJobItem()
                 job_item['title'] = job_title
                 job_item['company'] = job_company
                 job_item['location'] = job_location
                 job_item['description'] = job_description
                 job_item['source'] = 'glassdoor.com'
+                print("*********************************")
+                print(job_item)
                 
                 yield job_item
             
             # move to the next page of job list
             #next_page = driver.find_elements_by_css_selector('.pagingControls.cell.middle li')[-1]
-            next_page = driver.find_elements_by_css_selector('li.page a')[-1]
+            # next_page = driver.find_element_by_css_selector('.next a')
+            next_page = driver.find_elements_by_css_selector('.next a')[0]
             print('NEXT PAGE: %s' %next_page)
             if next_page:
                 yield scrapy.Request(next_page.get_attribute('href'), callback=self.parse)
